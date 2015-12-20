ed54f93283a1ee51bac5444ab4d4881cc88c2f70
lucius fox <mr.captain.awesome@gmail.com>
2015-12-20 00:35:21 -0500

    fb social sharing image

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -16,33 +16,16 @@ class Setting < ActiveRecord::Base
                 :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 	validates_attachment :additional_image, :content_type => {:content_type => /\Aimage\/.*\Z/}
 
-	has_attached_file :facebook_image,
-								:storage => :s3,
-								:s3_protocol => 'https',
-								:bucket => ENV['S3_BUCKET'],
-                :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	has_attached_file :facebook_image
 	validates_attachment :facebook_image, :content_type => {:content_type => /\Aimage\/.*\Z/}
 
-	has_attached_file :twitter_image, :storage => :s3,
-								:s3_protocol => 'https',
-								:bucket => ENV['S3_BUCKET'],
-                :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	has_attached_file :twitter_image
 	validates_attachment :twitter_image, :content_type => {:content_type => /\Aimage\/.*\Z/}
 
-	has_attached_file :pinterest_image, :storage => :s3,
-								:s3_protocol => 'https',
-								:bucket => ENV['S3_BUCKET'],
-                :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	has_attached_file :pinterest_image
 	validates_attachment :pinterest_image, :content_type => {:content_type => /\Aimage\/.*\Z/}	
 
-	has_attached_file :google_plus_image, :storage => :s3,
-								:s3_protocol => 'https',
-								:bucket => ENV['S3_BUCKET'],
-                :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	has_attached_file :google_plus_image
 	validates_attachment :google_plus_image, :content_type => {:content_type => /\Aimage\/.*\Z/}
 
 	after_save :clear_cache
