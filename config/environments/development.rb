ac0e9365360c5ad157237d0163e8c8a576ae26db
lucius fox <mr.captain.awesome@gmail.com>
2015-12-20 00:14:00 -0500

    fb social sharing image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,13 +41,5 @@ Rails.application.configure do
 
   # For better errors
   BetterErrors::Middleware.allow_ip! '192.168.0.100'
-  
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+
 end
